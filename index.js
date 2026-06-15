2e976a8d5f8f9c76985e592ecfe7d3938b25a626
Gustavo Rodrigues <rodrigues.gustavo20@escola.pr.gov.br>
2026-06-15 08:52:43 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// npm init
+// npm i express
+// instalar extenso RapidAPI Clientno VScode
+const express = require("express")
+const app = express()
+const port = 3000
+app.use(express.json())
+const fs = require('fs')
+app.post("/clientes", (req, res) => {
+    const cliente = req.body
+    try {
+        // abrir o arquivo
+        const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+        // adicionar o cliente
+        bd.push(cliente)
+        // salvar o arquivo
+        fs.writeFileSync("bd.json", JSON.stringify(bd), "utf8")
+        // resposta
+        res.status(201).json({resposta: "Cliente cadastrado!"})
+    } catch (erro) {
+        res.status(500).json({erro: erro.messagw})
+    }
+})
+
+app.listen(port, ()=>{
+    console.log("API executando na porta "+ port)
+})
