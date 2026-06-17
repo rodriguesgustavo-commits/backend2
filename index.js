1dd820c9b64e20f1fb48c9856035d28937bc1da7
Gustavo Rodrigues <rodrigues.gustavo20@escola.pr.gov.br>
2026-06-17 09:44:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,22 @@ app.post("/clientes", (req, res) => {
         // resposta
         res.status(201).json({resposta: "Cliente cadastrado!"})
     } catch (erro) {
-        res.status(500).json({erro: erro.messagw})
+        res.status(500).json({erro: erro.message})
+    }
+})
+
+app.get ("/clientes", (req, res) => {
+    try {
+        // abrir o arquivo
+        const bd = JSON.parse(fs.readFileSync("bd.json", "utf8"))
+        res.status(200).json({resposta: bd})
+    } catch (erro) {
+        res.status(500).json({erro: erro.message})
     }
 })
 
 app.listen(port, ()=>{
     console.log("API executando na porta "+ port)
 })
+
+// GET ​http://localhost:3000/clientes
